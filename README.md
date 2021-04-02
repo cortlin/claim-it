4285553c6be9fa2c03174b2e13be93ad7f2dcf6d
Cortlin <cortlin.sawyer@gmail.com>
2021-04-02 07:05:43 -0500

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+#Claim it Prototype
+
+Prototype to show the user flow of the court user when creating a new quick settlement.
+
+This prototype is also conencted to the twilio API to send messages to the plaintiff. 
---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+  const accountSid = 'ACccda32bc3a9ee3ba2b87ac6cbc0d2422';
+  const authToken = '5cadc5dba0b7a340e0aa3a928a3b438b';
+  const client = require('twilio')(accountSid, authToken);
+
+  client.studio.v1.flows('FWb063e3831ac34282a1025b40a61165c1')
+  .executions.create({ 
+    to: `+1${req.body.phoneNum}`,
+    from: '+17015994917',
+    parameters: JSON.stringify({name: req.body.name})})
+    .then(function(execution) { 
+      res.send(('It worked!: ', execution.sid));
+    }).catch(error => {
+      console.log(error);
+    });
+  
+})
+
+module.exports = router;
