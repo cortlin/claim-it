ea69ef23b59a678878721d5d588820858fd16aaa
Cortlin Sawyer <cortlin@forcefield.ai>
2021-04-04 17:28:19 -0500

    added settlement agreement at route /settlement

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const fs = require('fs');
 
-const indexRouter = require('./routes/index');
-const apiRouter = require('./api/index')
+const indexRouter = require("./routes/index");
+const apiRouter = require("./api/index");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(helmet());
 
-app.use(express.static(path.join(__dirname, 'pages')))
+app.use(express.static(path.join(__dirname, "pages")));
 
-app.use('/', indexRouter);
-app.use('/api', apiRouter);
+app.use("/", indexRouter);
+app.use("/api", apiRouter);
+
+app.get("/settlement", (req, res) => {
+  const data =fs.readFileSync('./contract.pdf');
+  res.contentType("application/pdf");
+  res.send(data);
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
