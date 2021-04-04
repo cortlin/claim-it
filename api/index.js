6785c7c6a6ca76a6a0d85b4e7b3194952323654c
Cortlin Sawyer <cortlin@forcefield.ai>
2021-04-04 13:37:44 -0500

    more changes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  const accountSid = 'ACccda32bc3a9ee3ba2b87ac6cbc0d2422';
-  const authToken = '5cadc5dba0b7a340e0aa3a928a3b438b';
+  const accountSid = process.env.TWILIO_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
   const client = require('twilio')(accountSid, authToken);
 
   client.studio.v1.flows('FWb063e3831ac34282a1025b40a61165c1')
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -14,8 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(cors());
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'pages')))
 
