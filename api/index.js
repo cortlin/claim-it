6658f50d8d572bfcfc7cbb8b684637c614fef5b4
Cortlin Sawyer <cortlin@forcefield.ai>
2021-04-04 12:34:29 -0500

    updates

--- api/index.js
+++ api/index.js
@@ -8,9 +8,9 @@ router.post('/', (req, res) => {
 
   client.studio.v1.flows('FWb063e3831ac34282a1025b40a61165c1')
   .executions.create({ 
-    to: `+1${req.body.phoneNum}`,
+    to: `+1${req.body.plaintiffPhone}`,
     from: '+17015994917',
-    parameters: JSON.stringify({name: req.body.name})})
+    parameters: JSON.stringify(req.body)})
     .then(function(execution) { 
       res.send(('It worked!: ', execution.sid));
     }).catch(error => {
